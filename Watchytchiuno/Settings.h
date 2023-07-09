7b52175881caf72c54ebe7179305fc977afc1689
Robin LoBuglio <B.LoBuglio@Gmail.com>
2023-07-09 12:57:48 -0700

    Fill out settings.h with los angeles information

--- Watchytchiuno/Settings.h
+++ Watchytchiuno/Settings.h
@@ -1,8 +1,26 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+//Weather Settings
+#define CITY_ID "5368361" // Los Angeles  https://openweathermap.org/current#cityid
+#define OPENWEATHERMAP_APIKEY "[get your key at openweathermap]" //use your own API key :)
+#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
+#define TEMP_UNIT "imperial" //metric = Celsius , imperial = Fahrenheit
+#define TEMP_LANG "en"
+#define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
+//NTP Settings
+#define NTP_SERVER "pool.ntp.org"
+#define GMT_OFFSET_SEC 3600 * -7 // PDT
 
 watchySettings settings{
+    .cityID = CITY_ID,
+    .weatherAPIKey = OPENWEATHERMAP_APIKEY,
+    .weatherURL = OPENWEATHERMAP_URL,
+    .weatherUnit = TEMP_UNIT,
+    .weatherLang = TEMP_LANG,
+    .weatherUpdateInterval = WEATHER_UPDATE_INTERVAL,
+    .ntpServer = NTP_SERVER,
+    .gmtOffset = GMT_OFFSET_SEC,
     .vibrateOClock = false,
 };
 
