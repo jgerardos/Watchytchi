429decc900658ed925ed64d54e3fed7968304d40
Robin LoBuglio <B.LoBuglio@Gmail.com>
2023-07-12 14:01:25 -0700

    Don't schedule close ups if sad

--- Watchytchiuno/Watchy_Watchytchi.cpp
+++ Watchytchiuno/Watchy_Watchytchi.cpp
@@ -303,8 +303,11 @@ void Watchytchi::drawWatchFace(){
 
     auto beforeAlertTs = nextAlertTs;
     /*# Interaction Alert #*/
-    // Schedule a new alert if we don't have one scheduled, if our current alert is expired, or if it is too far in the future due to a time change (TODO: are we correctly handling the last case?)
+    // Schedule a new alert if we don't have one scheduled, if our current alert is expired, or if it is too far in the future due to a time change
     if (nextAlertTs < 0 || currentEpochTime - nextAlertTs >= k_alertExpirationWindow  || nextAlertTs >= currentEpochTime + k_alertInterval * 2)
+      scheduleNextAlert();
+    // Reschedule alerts if we're unhappy
+    if (getHappyTier() <= HappyTier::Sad)
       scheduleNextAlert();
     // If the owner pressed the alert button while it was active, execute an animation
     if (isExecutingAlertInteraction)
